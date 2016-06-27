59c32d50044e2c977ea2bdb4dbe53b2461acb5b0
kammezh <kammezh.motorcars@hotmail.com>
2016-06-27 14:24:02 -0400

    Ajout de waifu.html Ajout de script.js Ajout de Beauty.png

---
+++ script.js
@@ -0,0 +1,7 @@
+function scriptButton() {
+    window.alert(firstname.value +" is a boss");
+    console.log("Le prenom est " + firstname.value);
+    console.log("Le nom est " + lastname.value);
+    console.log("Sa waifu est " + waifu.value);
+}
+
