52814e46b690d19c1b3af42ed1b205bcfdf32e81
Joel Cheong Yn <joel.cheong-yn@polymtl.ca>
2016-06-27 18:08:13 -0400

    node modules

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Configuration de la base de données MongoDB.
+var mongoose = require('mongoose');
+var utilisateur = 'joel';
+var mdp = 'joel@ds051534';
+var adresse = '.mongolab.com:51534/les-grottes-de-kalte';
+mongoose.connect('mongodb://' + utilisateur + ':' + mdp + adresse);
+
+
+
 function scriptButton() {
     window.alert(firstname.value +" is a boss");
     console.log("Le prenom est " + firstname.value);
